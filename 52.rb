f541bf86e5c7b9675bd64844c55ce3fd1aa76db3
RaghinRadhakrishnan <30748414+RaghinRadhakrishnan@users.noreply.github.com>
2018-02-21 16:21:34 +0530

    Create 52.rb

---
+++ 52.rb
@@ -0,0 +1,12 @@
+puts "Enter any number"
+a=gets.chomp.to_i
+puts "one"if(a==1)
+puts "two"if(a==2)
+puts "three"if(a==3)
+puts "four"if(a==4)
+puts "five"if(a==5)
+puts "six"if(a==6)
+puts "seven"if(a==7)
+puts "eight"if(a==8)
+puts "nine"if(a==9)
+puts "ten"if(a==10)
